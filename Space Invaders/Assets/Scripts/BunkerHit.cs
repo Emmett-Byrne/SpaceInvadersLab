9c0161faf6f0fe0b854e309dcb634eb43e6d3607
Oisin Wilson <oisinwilson@gmail.com>
2018-10-26 14:27:00 +0100

    bunkers

--- Space Invaders/Assets/Scripts/BunkerHit.cs
+++ Space Invaders/Assets/Scripts/BunkerHit.cs
@@ -3,21 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BunkerHit : MonoBehaviour {
-
-    private GameObject bunker;
-
-	void Start ()
-    {
-        bunker = this.GetComponent<GameObject>();
-	}
 	
     void OnTriggerEnter(Collider other)
     {
-
+        if (other.tag != "Boundary")
+        {
+            if (other.tag == "Shot")
+            {
+                if (this.transform.localScale.x > 0.4 && 
+                    this.transform.localScale.y > 0.4)
+                {
+                    this.gameObject.transform.localScale = 
+                        new Vector3(this.transform.localScale.x * 0.9f,
+                        this.transform.localScale.y * 0.9f,
+                        1);
+                }
+                else
+                {
+                    Destroy(this.gameObject);
+                }
+                Destroy(other.gameObject);
+            }
+        }
     }
-
-	void Update ()
-    {
-	
-	}
 }
