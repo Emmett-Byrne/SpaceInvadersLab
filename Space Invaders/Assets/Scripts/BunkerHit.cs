c42123cca57e9b056d00fc6dd4f08b3bd9848e32
Oisin Wilson <oisinwilson@gmail.com>
2018-10-26 13:31:43 +0100

    Added bunker object (incomplete)

---
+++ Space Invaders/Assets/Scripts/BunkerHit.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunkerHit : MonoBehaviour {
+
+    private GameObject bunker;
+
+	void Start ()
+    {
+        bunker = this.GetComponent<GameObject>();
+	}
+	
+    void OnTriggerEnter(Collider other)
+    {
+
+    }
+
+	void Update ()
+    {
+	
+	}
+}
